c0a0e2150f68907dfbf34d7bb40236a5696a2e7b
Angelica <Angelica137@users.noreply.github.com>
2024-04-07 15:58:10 +0100

    range_extraction returns string with ranges, red

--- tests/test_range_extraction.py
+++ tests/test_range_extraction.py
@@ -7,3 +7,7 @@ def test_range_extaction_returns_1():
 
 def test_range_extaction_returns_1_2_4_6():
     assert range_extraction([1, 2, 4, 6]) == "1,2,4,6"
+
+
+def test_range_extaction_returns_1_2_3_4_6():
+    assert range_extraction([1, 2, 3, 4, 6]) == "1-4,6"
