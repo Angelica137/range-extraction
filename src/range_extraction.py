062275817ce3d5a7e430a034db0d1c5399c1f7db
Angelica <Angelica137@users.noreply.github.com>
2024-04-08 17:59:51 +0100

    return correct range

--- src/range_extraction.py
+++ src/range_extraction.py
@@ -1,5 +1,24 @@
 def range_extraction(args: list) -> str:
-    solution = ""
-    for i in args:
-        solution += str(i) + ","
-    return solution[:-1]
+    solution = []
+    start = args[0]
+    end = args[0]
+    for i in args[1:]:
+        if i == end + 1:
+            end = i
+        else:
+            if end == start + 1:
+                solution.append(str(start))
+                solution.append(str(end))
+            elif end == start:
+                solution.append(str(start))
+            else:
+                solution.append(f"{start}-{end}")
+            start = end = i
+    if end == start:
+        solution.append(str(start))
+    elif end == start + 1:
+        solution.append(str(start))
+        solution.append(str(end))
+    else:
+        solution.append(f"{start}-{end}")
+    return ",".join(solution)
